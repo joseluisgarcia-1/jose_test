cb4bd31e82b09c25552e65eae29700aa4a37f8d9
joseluisgarcia-1 <jlgarcia002@misena.edu.co>
2021-11-03 01:38:37 -0500

    new settings html

--- jose_test/apps/products/views.py
+++ jose_test/apps/products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import ListView, CreateView, DeleteView, UpdateView
-from apps.products.models import Store, Product
+from apps.products.models import Marca, Product
 from django.contrib.auth.models import User
 # Create your views here.
 from apps.products.forms import MarcaForm, ProductForm
@@ -43,17 +43,17 @@ class ProductCreate(CreateView):
 
 class ProductUpdate(UpdateView):
     model = Product
-    second_model = Store
+    second_model = Marca
     template_name = 'products/products_form.html'
     form_class = ProductForm
     second_form_class = MarcaForm
     success_url = reverse_lazy('products:products_listar')
 
-    def get_context_data(self, **kwargs):
+"""    def get_context_data(self, **kwargs):
         context = super(ProductUpdate, self).get_context_data(**kwargs)
         pk = self.kwargs.get('pk', 0)
-        products = self.model.objects.get(id=pk)
-        store = self.second_model.objects.get(id=products.store_id)
+        stores = self.model.objects.get(id=pk)
+        store = self.second_model.objects.get(id=stores.store_id)
         if 'form' not in context:
             context['form'] = self.form_class()
         if 'form2' not in context:
@@ -63,17 +63,17 @@ class ProductUpdate(UpdateView):
     
     def post(self, request, *args, **kwargs):
         self.object = self.get_object
-        id_products = kwargs['pk']
-        products = self.model.objects.get(id=id_products)
-        store = self.second_model.objects.get(id=products.store_id)
-        form = self.form_class(request.POST, instance=products)
-        form2 = self.second_form_class(request.POST, instance=store)
+        id_store = kwargs['pk']
+        stores = self.model.objects.get(id=id_stores)
+        store = self.second_model.objects.get(id=store.name_id)
+        form = self.form_class(request.POST, instance=store)
+        form2 = self.second_form_class(request.POST, instance=product)
         if form.is_valid() and form2.is_valid():
             form.save()
             form2.save()
             return HttpResponseRedirect(self.get_success_url())
         else:
-            return HttpResponseRedirect(self.get_success_url())
+            return HttpResponseRedirect(self.get_success_url())"""
 
 class ProductDelete(DeleteView):
     model = Product
