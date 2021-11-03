564476ae3da463fb719b9d797b87d3e17ba6a475
joseluisgarcia-1 <jlgarcia002@misena.edu.co>
2021-11-02 22:24:46 -0500

    ajuste models

--- jose_test/apps/products/models.py
+++ jose_test/apps/products/models.py
@@ -14,11 +14,12 @@ from apps.stores.models import Store
     
     def __str__(self):
         return '{} {}'.format(self.name)"""
-
+#modelo marca de un producto
 class Marca(models.Model):
     name = models.CharField(max_length = 100)
     store = models.ForeignKey(Store, null=True, blank=True, on_delete=models.CASCADE)
 
+#model producto
 class Product(models.Model):
     TITLES_STATES =[
         ('Disponible', 'Disponible'),
@@ -30,8 +31,8 @@ class Product(models.Model):
     status = models.CharField(max_length = 50, choices=TITLES_STATES)
     price = models.DecimalField(max_digits = 6, decimal_places = 2)
     stock = models.IntegerField()
+    marca = models.ManyToManyField(Marca)
     #store = models.ForeignKey(Store, null=True, blank=True, on_delete=models.CASCADE)
-    #marca = models.ManyToManyField(Marca)
     #image = models.ImageField(upload_to='products', null = True)
     #categories = models.ManyToManyField(Category, null = True, blank = True)
     #marca = models.CharField(max_length = 100)
