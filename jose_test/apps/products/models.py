bf7180d65e34d998667bcaad28bb5b2fe59d6354
joseluisgarcia-1 <jlgarcia002@misena.edu.co>
2021-11-02 20:45:30 -0500

    create models

--- jose_test/apps/products/models.py
+++ jose_test/apps/products/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+"""class Category(models.Model):
+    name = models.CharField(max_length = 100)
+    description = models.TextField(max_length = 100)
+
+    def __str__(self):
+        return '{} {}'.format(self.name, self.description)"""
+
+
+"""class Marca(model.Model):
+    name = models.CharField(max_length = 100)
+    
+    def __str__(self):
+        return '{} {}'.format(self.name)"""
+
+class Product(models.Model):
+    TITLES_STATES =[
+        ('Disponible', 'Disponible'),
+        ('Agotado', 'Agotado'),
+        ('Fabrica', 'Fabrica'),
+    ]
+    name = models.CharField(max_length = 100)
+    description = models.TextField(max_length = 100)
+    status = models.CharField(max_length = 50, choices=TITLES_STATES)
+    price = models.DecimalField(max_digits = 6, decimal_places = 2)
+    stock = models.IntegerField()
+    store = models.ForeignKey(Store, null=True, blank=True, on_delete=models.CASCADE)
+    #image = models.ImageField(upload_to='products', null = True)
+    #categories = models.ManyToManyField(Category, null = True, blank = True)
+    #marca = models.CharField(max_length = 100)
+    
--- jose_test/apps/stores/models.py
+++ jose_test/apps/stores/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Store(models.Model):
+    nit = models.CharField(max_length = 100)
+    nombre_empresa = models.CharField(max_length = 100)
+    nombre_comercial = models.CharField(max_length = 100)
+    direccion = models.CharField(max_length = 100)
+    telefono = models.CharField(max_length = 100)
+    correo = models.EmailField()
+    sitio_web = models.URLField(max_length = 100)
+    pais = models.CharField(max_length = 100)
+    estado = models.CharField(max_length = 100)
+    ciudad = models.CharField(max_length = 100)
+
+class Marca(models.Model):
+    name = models.ForeignKey(Store, null=True, blank=True, on_delete=models.CASCADE)
